eb452b1378e1e43ed7a3652cd0268eb3637b087d
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-06-08 05:36:51 +0600

    Added user insert api

--- index.js
+++ index.js
@@ -1,22 +1,69 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// configure express app
+// Configure express app
 const app = express();
 
-// port cofigure
+// Port cofigure
 const port = process.env.PORT || 5000;
 
-// use middleware
+// Use middleware
 app.use(express.json());
 app.use(cors());
 
-// main api
+// Main api
 app.get("/", (req, res) => {
   res.send("Harmonic server is running ....");
 });
 
-// port listener
+// Mongodb url
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vp8crsv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Harmonic db collections
+    const usersCollection = client.db("harmonicDB").collection("users");
+
+    // Users API
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Port listener
 app.listen(port, () => {
   console.log(`Harmonic server is running on port ${port}`);
 });
